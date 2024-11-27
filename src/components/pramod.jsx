aedcfc8902d983fdae4f6d1ff6cda02a6716a7d0
AdiGolandaj <adigolandaj786@gmail.com>
2024-11-28 00:08:32 +0530

    important push just updating some text sizes and responsiveness

--- src/components/pramod.jsx
+++ src/components/pramod.jsx
@@ -18,10 +18,10 @@ const pramod = () => {
           {/* Mobile gradient overlay */}
           <div className="md:hidden absolute inset-0 h-full p-6 pb-6 flex flex-col-reverse justify-start items-start bg-gradient-to-b from-transparent via-transparent to-gray-900">
             <h3 className="w-full font-bold text-2xl text-white leading-tight mb-2">
-                Dr. P. K. Shrivastava
+                Dr. Pramod Kumar
             </h3>
             <h4 className="w-full text-xl text-gray-100 leading-tight">
-                Principal of ISBM CoE
+                President of ISB&M Group
             </h4>
           </div>
           {/* Angled Shape */}
@@ -38,13 +38,13 @@ const pramod = () => {
         <div className="z-10 order-2 md:order-1 w-full h-full md:w-3/5 flex items-center -mt-6 md:mt-0">
           <div className="p-8 md:pr-18 md:pl-14 md:py-12 mx-2 md:mx-0 h-full bg-white rounded-lg md:rounded-none md:rounded-l-lg shadow-xl md:shadow-none">
             <h4 className="hidden md:block text-xl text-gray-400">
-              Principal of ISBM CoE
+                President of ISB&M Group
             </h4>
             <h3 className="hidden md:block font-bold text-2xl text-gray-700 ">
-              Dr. P. K. Shrivastava
+                Dr. Pramod Kumar
             </h3>
             <p className="text-gray-600 text-justify md:text-custom-xs lg:text-sm xl:text-base 2xl:text-lg">
-            Dr. P.K. Shrivastava, Principal of ISBM CoE, is a Lifetime Member of ISTE, an approved Ph.D. guide at Pune University, and a member of the Avishkar Research Committee at SPPU. Having served as Head of Department at various engineering colleges, IEEE Branch Counsellor, Research Coordinator at PVPIT Pune, he has also authored numerous research papers.</p>
+            Dr. Pramod Kumar holds a Ph.D. in Organizational Behaviour from IIT Mumbai. Having previously worked with IIM Ahmedabad, he also consulted for over 80 companies worldwide. An author of over 100 research papers, cases, and management games, his work is quoted internationally in textbooks and journals. Served on Government of India committees on management education.</p>
             <a
               className="flex items-baseline mt-3 text-blue-600 hover:text-blue-900 focus:text-blue-900"
               href="#"
