7c68893c7b4e5e873a8d3608d2590bbd94c2d944
AdiGolandaj <adigolandaj786@gmail.com>
2024-12-28 14:47:44 +0530

    wavebelhero works now ig

--- src/components/wavebelHero.jsx
+++ src/components/wavebelHero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const wavebelHero = () => {
   return (
-    <div className="aspect-[900/150] w-full bg-cover bg-center bg-no-repeat" style={{ backgroundImage: "url('./public/svg/wave1.svg')" }}>
+    <div className="aspect-[900/150] w-full bg-cover bg-center bg-no-repeat" style={{ backgroundImage: "url('./svg/wave1.svg')" }}>
     </div>
   )
 }
